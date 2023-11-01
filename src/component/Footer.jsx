2af3c8e171426d125d331cc4d2d72d136b5e7648
David <david123rana@gmail.com>
2023-11-01 16:50:40 +0530

    i have created

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,14 +3,19 @@ import './Footer.css';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="diwali-message">
-        <p>On behalf of Team khatimacloud uff Me and Bro, Wishing you a Happy Diwali filled with love, light, and joy!</p>
+    <div className="page">
+      <div className="content">
+        {/* Your page content goes here */}
       </div>
-      <div className="love-symbol">
-        <span role="img" aria-label="Love Symbol">❤️</span>
-      </div>
-    </footer>
+      <footer className="footer">
+        <div className="diwali-message">
+          <p>On behalf of Team khatimacloud uff Me and Bro, Wishing you a Happy Diwali filled with love, light, and joy!</p>
+        </div>
+        <div className="love-symbol">
+          <span role="img" aria-label="Love Symbol">❤️</span>
+        </div>
+      </footer>
+    </div>
   );
 };
 
